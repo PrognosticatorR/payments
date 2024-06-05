70173db26d728ebf792d68008f0d216bf3077eaf
Devesh Rawat <devesh@roadzen.io>
2024-06-06 02:52:43 +0530

    chore: setup app.js

--- src/app.js
+++ src/app.js
@@ -1,16 +1,26 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const dotenv = require("dotenv");
-const app = express();
+const morgan = require("morgan");
+const helmet = require("helmet");
+const compression = require("compression");
+const cors = require("cors");
+
 const adminRoutes = require("./routes/adminRoutes");
 const balanceRoutes = require('./routes/balanceRoutes');
 const contractRoutes = require("./routes/contractRoutes");
 const jobRoutes = require("./routes/jobRoutes");
 
+const app = express();
 dotenv.config();
 
 // Middleware
-app.use(bodyParser.json());
+app.use(bodyParser.json({ limit: '5mb' })); // Set JSON body limit to 5 mb
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(morgan("dev"))
+app.use(helmet());
+app.use(compression());
+app.use(cors({ origin: ["http://localhost:3001"] }));
 
 // Routes
 app.use("/admin", adminRoutes);
--- src/server.js
+++ src/server.js
@@ -1,11 +1,23 @@
 const app = require("./app");
 const PORT = process.env.PORT || 3001;
-
+const cluster = require("cluster");
+const os = require("os");
 async function init() {
   try {
-    app.listen(PORT, () => {
-      console.log("Express App Listening on Port 3001");
-    });
+    if (cluster.isMaster) {
+      const numCPUs = os.cpus().length;
+      console.log(`Master ${process.pid} is running`);
+      for (let i = 0; i < numCPUs; i++) {
+        cluster.fork();
+      }
+      cluster.on('exit', (worker) => {
+        console.log(`Worker ${worker.process.pid} died`);
+      });
+    } else {
+      app.listen(PORT, () => {
+        console.log(`Express server is now running on process ${process.pid}, listening on port ${PORT}`);
+      });
+    }
   } catch (error) {
     console.log(error);
     console.error(`An error occurred: ${JSON.stringify(error)}`);
